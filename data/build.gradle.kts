0d64368c33dd0343a5dc96d452628e40bcb86101
JeonK1 <kan5016@naver.com>
2025-05-21 01:22:17 +0900

    [#50] Feature : proto datastore 의존성 추가

--- data/build.gradle.kts
+++ data/build.gradle.kts
@@ -1,6 +1,9 @@
+import com.google.protobuf.gradle.id
+
 plugins {
     id("dhc.module")
     id("dhc.hilt")
+    alias(libs.plugins.protobuf)
 }
 
 android {
@@ -12,7 +15,28 @@ dependencies {
 
     implementation(libs.bundles.network)
 
+    implementation(libs.bundles.proto.datastore)
+
     debugImplementation(libs.bundles.flipper)
 
     testImplementation(libs.junit)
+}
+
+protobuf {
+    protoc {
+        artifact = "com.google.protobuf:protoc:3.25.0"
+    }
+
+    // Generates the java Protobuf-lite code for the Protobufs in this project. See
+    // https://github.com/google/protobuf-gradle-plugin#customizing-protobuf-compilation
+    // for more information.
+    generateProtoTasks {
+        all().forEach { task ->
+            task.plugins {
+                id("java") {
+                    option("lite")
+                }
+            }
+        }
+    }
 }
