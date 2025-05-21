f2564d7a6c38e5aed9e5115b2956297ab2b7565e
JeonK1 <kan5016@naver.com>
2025-05-22 00:07:47 +0900

    [#51] Feature : Screen 스켈레톤 코드 추가

---
+++ presentation/intro/src/main/java/com/dhc/intro/tutorial/TutorialScreen.kt
@@ -0,0 +1,15 @@
+package com.dhc.intro.tutorial
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+
+@Composable
+fun TutorialScreen() {
+
+}
+
+@Preview
+@Composable
+private fun TutorialScreenPreview() {
+    TutorialScreen()
+}
