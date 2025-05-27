6807957a3f7060be618d483dc4523125c98cf6e4
ddyeon <cab05102@naver.com>
2025-05-27 09:27:22 +0900

    [#65] chore : UUID 값 받아오기

--- app/src/main/java/com/dhc/dhcandroid/MainActivity.kt
+++ app/src/main/java/com/dhc/dhcandroid/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.dhc.dhcandroid
 
+import android.annotation.SuppressLint
 import android.os.Bundle
+import android.provider.Settings
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
@@ -13,10 +15,17 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+        getUUID()
         setContent {
             DHCAndroidTheme {
                 HomeRoute()
             }
         }
+    }
+
+
+    @SuppressLint("HardwareIds")
+    private fun getUUID() {
+        val userId = Settings.Secure.getString(contentResolver, Settings.Secure.ANDROID_ID)
     }
 }
