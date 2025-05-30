7559cdb6a464b0c8ace12fabac937727913c55a3
JeonK1 <kan5016@naver.com>
2025-05-31 00:59:23 +0900

    [#67] Rename : Color, Theme, Type 을 designsystem 모듈로 이동

--- app/src/main/java/com/dhc/dhcandroid/MainActivity.kt
+++ app/src/main/java/com/dhc/dhcandroid/MainActivity.kt
@@ -5,7 +5,7 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import com.dhc.dhcandroid.navigation.DhcApp
-import com.dhc.dhcandroid.ui.theme.DHCAndroidTheme
+import com.dhc.designsystem.DHCAndroidTheme
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
