55bb57afa6fe4280049a42859b803c788e0ab0ff
JeonK1 <kan5016@naver.com>
2025-05-31 01:35:23 +0900

    [#67] Delete : 불필요한 주석제거

--- presentation/intro/src/main/java/com/dhc/intro/splash/SplashContract.kt
+++ presentation/intro/src/main/java/com/dhc/intro/splash/SplashContract.kt
@@ -6,23 +6,13 @@ import com.dhc.presentation.mvi.UiState
 
 class SplashContract {
 
-    /**
-     * 현재 화면에 필요한 상태들을 모아둔다.
-     */
     data class State(
         val isSplashFinished: Boolean = false,
     ) : UiState
 
-
-    /**
-     * 액션 정의
-     */
     sealed interface Event : UiEvent {
         data object SplashFinish : Event
     }
 
-    /**
-     * SideEffect로 발생되는 이벤트를 정의
-     */
     sealed interface SideEffect : UiSideEffect
 }
