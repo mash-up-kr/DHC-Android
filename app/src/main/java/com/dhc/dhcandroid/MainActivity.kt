88ad8ea18b471d5562d3f097dc298f13d8fc3221
JeonK1 <kan5016@naver.com>
2025-05-31 01:34:06 +0900

    [#71] Fix : detekt 수정

--- app/src/main/java/com/dhc/dhcandroid/MainActivity.kt
+++ app/src/main/java/com/dhc/dhcandroid/MainActivity.kt
@@ -4,8 +4,8 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import com.dhc.dhcandroid.navigation.DhcApp
 import com.dhc.designsystem.DHCAndroidTheme
+import com.dhc.dhcandroid.navigation.DhcApp
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
