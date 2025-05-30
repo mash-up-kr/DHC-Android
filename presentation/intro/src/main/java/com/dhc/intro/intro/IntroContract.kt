371aebb8d7f17500aaac5be7f11687ecbb4de186
JeonK1 <kan5016@naver.com>
2025-05-31 01:33:36 +0900

    [#71] Delete : 불필요한 주석 제거

--- presentation/intro/src/main/java/com/dhc/intro/intro/IntroContract.kt
+++ presentation/intro/src/main/java/com/dhc/intro/intro/IntroContract.kt
@@ -6,9 +6,6 @@ import com.dhc.presentation.mvi.UiState
 
 class IntroContract {
 
-    /**
-     * 현재 화면에 필요한 상태들을 모아둔다.
-     */
     data class State(
         val page: Int = 0,
         val totalPage: Int = 1,
@@ -16,16 +13,9 @@ class IntroContract {
         val isPageFinished = page == totalPage
     }
 
-
-    /**
-     * 액션 정의
-     */
     sealed interface Event: UiEvent {
         data object ClickNextButton : Event
     }
 
-    /**
-     * SideEffect로 발생되는 이벤트를 정의
-     */
     sealed interface SideEffect : UiSideEffect
 }
