2df4472e2baf0881c69dd638596c9f3504ed10b4
ddyeon <cab05102@naver.com>
2025-06-05 09:35:43 +0900

    [#97] fix : 컬러 정의 data class에서 object로 수정

--- core/designsystem/src/main/kotlin/com/dhc/designsystem/Color.kt
+++ core/designsystem/src/main/kotlin/com/dhc/designsystem/Color.kt
@@ -3,42 +3,42 @@ package com.dhc.designsystem
 import androidx.compose.ui.graphics.Color
 
 val colors = DhcColors(
-    surface = SurfaceColor(),
-    accent = AccentColor(),
+    surface = SurfaceColor,
+    accent = AccentColor,
     text = TextColors(
-        textMain = SurfaceColor().neutral30,
-        textBodyPrimary = SurfaceColor().neutral100,
-        textHighLightsSecondary = AccentColor().violet200,
-        textHighLightsPrimary = AccentColor().violet400
+        textMain = SurfaceColor.neutral30,
+        textBodyPrimary = SurfaceColor.neutral100,
+        textHighLightsSecondary = AccentColor.violet200,
+        textHighLightsPrimary = AccentColor.violet400
     ),
     background = BackgroundColors(
-        backgroundMain = SurfaceColor().neutral900
+        backgroundMain = SurfaceColor.neutral900
     )
 )
 
-data class SurfaceColor(
-    val neutral30: Color = Color(0xFFF4F4F5),
-    val neutral50: Color = Color(0xFFE6EDF8),
-    val neutral100: Color = Color(0xFFD7E1EE),
-    val neutral200: Color = Color(0xFFA5B2C5),
-    val neutral300: Color = Color(0xFF7B8696),
-    val neutral400: Color = Color(0xFFA5B2C5),
-    val neutral500: Color = Color(0xFF7B8696),
-    val neutral700: Color = Color(0xFF1F2127),
-    val neutral800: Color = Color(0xFF17191F),
+data object SurfaceColor {
+    val neutral30: Color = Color(0xFFF4F4F5)
+    val neutral50: Color = Color(0xFFE6EDF8)
+    val neutral100: Color = Color(0xFFD7E1EE)
+    val neutral200: Color = Color(0xFFA5B2C5)
+    val neutral300: Color = Color(0xFF7B8696)
+    val neutral400: Color = Color(0xFFA5B2C5)
+    val neutral500: Color = Color(0xFF7B8696)
+    val neutral700: Color = Color(0xFF1F2127)
+    val neutral800: Color = Color(0xFF17191F)
     val neutral900: Color = Color(0xFF0F1114)
-)
+}
 
-data class AccentColor(
-    val violet50: Color = Color(0xFFE6E8FF),
-    val violet100: Color = Color(0xFFCDD1F2),
-    val violet200: Color = Color(0xFFB5BAEB),
-    val violet300: Color = Color(0xFF939BE2),
-    val violet400: Color = Color(0xFF5E69D4),
-    val violet500: Color = Color(0xFF5660C1),
-    val violet600: Color = Color(0xFF414BAE),
+data object AccentColor {
+    val violet50: Color = Color(0xFFE6E8FF)
+    val violet100: Color = Color(0xFFCDD1F2)
+    val violet200: Color = Color(0xFFB5BAEB)
+    val violet300: Color = Color(0xFF939BE2)
+    val violet400: Color = Color(0xFF5E69D4)
+    val violet500: Color = Color(0xFF5660C1)
+    val violet600: Color = Color(0xFF414BAE)
     val violet700: Color = Color(0xFF343FA6)
-)
+}
 
 data class TextColors(
     val textMain: Color,
