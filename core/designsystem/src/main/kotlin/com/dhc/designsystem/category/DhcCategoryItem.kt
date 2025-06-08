03494177df4cc5db280e0dc1b2aed818a8091955
JeonK1 <kan5016@naver.com>
2025-06-09 00:24:15 +0900

    [#112] Feature : CategoryItem 개발

---
+++ core/designsystem/src/main/kotlin/com/dhc/designsystem/category/DhcCategoryItem.kt
@@ -0,0 +1,112 @@
+package com.dhc.designsystem.category
+
+import androidx.annotation.DrawableRes
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.tooling.preview.PreviewParameter
+import androidx.compose.ui.tooling.preview.PreviewParameterProvider
+import androidx.compose.ui.unit.dp
+import com.dhc.designsystem.DhcTheme
+import com.dhc.designsystem.DhcTypoTokens
+import com.dhc.designsystem.LocalDhcColors
+import com.dhc.designsystem.R
+import com.dhc.designsystem.SurfaceColor
+import com.dhc.designsystem.check.DhcCheck
+import com.dhc.designsystem.check.model.DhcCheckStyle
+
+@Composable
+fun DhcCategoryItem(
+    name: String,
+    isChecked: Boolean,
+    @DrawableRes iconResource: Int,
+    modifier: Modifier = Modifier,
+) {
+    val colors = LocalDhcColors.current
+
+    Column(
+        verticalArrangement = Arrangement.spacedBy(8.dp),
+        modifier = modifier
+            .background(
+                color = if (isChecked) colors.background.backgroundBadgePrimary else SurfaceColor.neutral700,
+                shape = RoundedCornerShape(12.dp)
+            )
+            .padding(16.dp),
+    ) {
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(24.dp),
+        ) {
+            Text(
+                text = name,
+                style = DhcTypoTokens.TitleH5,
+                color = if (isChecked) colors.text.textHighLightsSecondary else colors.text.textBodyPrimary,
+                modifier = Modifier.weight(1f),
+            )
+            if (isChecked) {
+                DhcCheck(
+                    isChecked = true,
+                    isEnabled = true,
+                    dhcCheckStyle = DhcCheckStyle(
+                        containerSize = 24.dp,
+                        iconSize = 14.dp,
+                    ),
+                )
+            }
+        }
+        Icon(
+            painter = painterResource(iconResource),
+            contentDescription = null,
+            modifier = Modifier.size(36.dp),
+        )
+    }
+}
+
+private class DhcCategoryPreviewProvider : PreviewParameterProvider<DhcCategoryPreviewProvider.Parameter> {
+    override val values = sequenceOf(
+        Parameter(
+            name = "식음료",
+            isChecked = false,
+            iconResource = R.drawable.check,
+        ),
+        Parameter(
+            name = "식음료",
+            isChecked = true,
+            iconResource = R.drawable.check,
+        ),
+    )
+
+    data class Parameter(
+        val name: String,
+        val isChecked: Boolean,
+        @DrawableRes val iconResource: Int,
+    )
+}
+
+@Preview
+@Composable
+private fun DhcCategoryPreview(
+    @PreviewParameter(DhcCategoryPreviewProvider::class)
+    parameter: DhcCategoryPreviewProvider.Parameter,
+) {
+    DhcTheme {
+        DhcCategoryItem(
+            name = parameter.name,
+            isChecked = parameter.isChecked,
+            iconResource = parameter.iconResource,
+        )
+    }
+}
