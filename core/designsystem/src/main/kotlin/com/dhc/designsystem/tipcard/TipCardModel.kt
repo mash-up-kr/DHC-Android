1d1bb9bd0709b2ba50eb8a17bba8a8226e3b0627
ddyeon <cab05102@naver.com>
2025-06-11 09:10:12 +0900

    [#126] Fix : 컬러변경 exception 처리

--- core/designsystem/src/main/kotlin/com/dhc/designsystem/tipcard/TipCardModel.kt
+++ core/designsystem/src/main/kotlin/com/dhc/designsystem/tipcard/TipCardModel.kt
@@ -2,6 +2,7 @@ package com.dhc.designsystem.tipcard
 
 import androidx.compose.ui.graphics.Color
 import androidx.core.graphics.toColorInt
+import com.dhc.designsystem.SurfaceColor
 
 /**
  * 임시 UI모델로 서버 리스펀스에 따라 추후 변경
@@ -15,5 +16,9 @@ data class TipCardModel(
 
 
 fun hexToColor(hex: String): Color {
-    return Color(hex.toColorInt())
+    return try {
+        Color(hex.toColorInt())
+    } catch (e: IllegalArgumentException) {
+        SurfaceColor.neutral100
+    }
 }
