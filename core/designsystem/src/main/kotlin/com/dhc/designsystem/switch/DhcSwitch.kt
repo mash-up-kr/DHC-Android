f1d215ddec4a3df3c2035c466f3d5e749d840163
fbghgus123 <fbghgus123@naver.com>
2025-06-21 11:26:27 +0900

    [#189] Feature : 디자인 컴포넌트 Switch border 추가

--- core/designsystem/src/main/kotlin/com/dhc/designsystem/switch/DhcSwitch.kt
+++ core/designsystem/src/main/kotlin/com/dhc/designsystem/switch/DhcSwitch.kt
@@ -4,6 +4,7 @@ import androidx.compose.animation.animateColorAsState
 import androidx.compose.animation.core.animateDpAsState
 import androidx.compose.animation.core.tween
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.height
@@ -20,6 +21,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.tooling.preview.PreviewParameter
 import androidx.compose.ui.unit.IntOffset
@@ -58,6 +60,11 @@ fun DhcSwitch(
             .height(switchSize.height)
             .clip(RoundedCornerShape(switchSize.radius))
             .background(backgroundColor)
+            .border(
+                width = if (isEnabled) 1.dp else 0.dp,
+                color = DhcSwitchColor.getColor(isOn, isEnabled).borderColor ?: Color.Transparent,
+                shape = RoundedCornerShape(switchSize.radius),
+            )
             .clickable {
                 if (isEnabled) {
                     onToggle(!isOn)
