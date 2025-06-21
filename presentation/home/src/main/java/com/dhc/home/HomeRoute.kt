216faa326ef144b228cd0fce899c843c6deb5e51
ddyeon <cab05102@naver.com>
2025-06-21 23:29:15 +0900

    [#172] Chore : import 수정

--- presentation/home/src/main/java/com/dhc/home/HomeRoute.kt
+++ presentation/home/src/main/java/com/dhc/home/HomeRoute.kt
@@ -1,6 +1,5 @@
 package com.dhc.home
 
-import androidx.compose.foundation.rememberScrollState
 import androidx.compose.runtime.Composable
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.dhc.home.ui.HomeScreen
