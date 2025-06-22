fb71133f66ac56af299c2e5b09ce1948e66534b1
fbghgus123 <fbghgus123@naver.com>
2025-06-22 16:04:42 +0900

    [#79] Feature : 마이 페이지 화면 베이스 코드 작성

---
+++ presentation/mypage/src/main/kotlin/com/dhc/mypage/ui/MyPageScreen.kt
@@ -0,0 +1,13 @@
+package com.dhc.mypage.ui
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import com.dhc.mypage.MyPageContract.State
+
+@Composable
+fun MyPageScreen(
+    state: State,
+    modifier: Modifier = Modifier,
+) {
+
+}
