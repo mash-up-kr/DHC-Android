cd11cea2961567adb67a0896c78862fec241bdf3
ddyeon <cab05102@naver.com>
2025-06-23 16:07:26 +0900

    [#193] Fix : 리뷰 반영

--- core/designsystem/src/main/kotlin/com/dhc/tooltip/DhcTooltip.kt
+++ core/designsystem/src/main/kotlin/com/dhc/tooltip/DhcTooltip.kt
@@ -9,6 +9,7 @@ import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Text
 import androidx.compose.material3.TooltipBox
 import androidx.compose.material3.TooltipDefaults
+import androidx.compose.material3.TooltipState
 import androidx.compose.material3.rememberTooltipState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.rememberCoroutineScope
@@ -22,6 +23,7 @@ import com.dhc.common.drawBalloonTail
 import com.dhc.designsystem.DhcTheme
 import com.dhc.designsystem.DhcTypoTokens
 import com.dhc.designsystem.LocalDhcColors
+import com.dhc.designsystem.R
 import com.dhc.designsystem.SurfaceColor
 import kotlinx.coroutines.launch
 
@@ -30,10 +32,9 @@ import kotlinx.coroutines.launch
 fun DhcTooltip(
     tooltipText: String,
     modifier: Modifier = Modifier,
-    isPersistent: Boolean = true,
+    tooltipState: TooltipState = rememberTooltipState(),
+    content: @Composable () -> Unit,
 ) {
-    val tooltipState = rememberTooltipState(isPersistent = isPersistent)
-    val coroutineScope = rememberCoroutineScope()
     val colors = LocalDhcColors.current
 
     TooltipBox(
@@ -59,26 +60,27 @@ fun DhcTooltip(
         },
         state = tooltipState
     ) {
-        Image(
-            modifier = Modifier.clickable {
-                coroutineScope.launch {
-                    tooltipState.show()
-                }
-            },
-            painter = painterResource(com.dhc.designsystem.R.drawable.ico_info_circle),
-            contentDescription = "information",
-            colorFilter = ColorFilter.tint(SurfaceColor.neutral400)
-        )
+        content()
     }
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Preview
 @Composable
 fun PreviewDhcTooltip() {
     DhcTheme {
         DhcTooltip(
+            tooltipState = rememberTooltipState(),
             tooltipText = "좋은 금융 습관 형성을 위해\n" +
                     "2주간 매일 진행하는 미션이에요!",
+            content = {
+                Image(
+                    modifier = Modifier.clickable {},
+                    painter = painterResource(R.drawable.ico_info_circle),
+                    contentDescription = "information",
+                    colorFilter = ColorFilter.tint(SurfaceColor.neutral400)
+                )
+            }
         )
     }
 }
