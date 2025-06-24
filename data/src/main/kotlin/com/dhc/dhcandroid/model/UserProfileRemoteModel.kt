b76739af346a85fd6ea6f625b72bb4d6d92575eb
fbghgus123 <fbghgus123@naver.com>
2025-06-24 21:15:01 +0900

    [#208] Feature : UserProfileRemoteModel 생성

---
+++ data/src/main/kotlin/com/dhc/dhcandroid/model/UserProfileRemoteModel.kt
@@ -0,0 +1,59 @@
+package com.dhc.dhcandroid.model
+
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class UserProfileRemoteModel(
+    val id: String = "",
+    val userToken: String = "",
+    val gender: Gender = Gender.MALE,
+    val birthDate: BirthDate? = null,
+    val birthTime: String = "",
+    val preferredMissionCategoryList: List<MissionCategory> = emptyList(),
+)
+
+@Serializable
+enum class Gender {
+    @SerialName("MALE")
+    MALE,
+
+    @SerialName("FEMALE")
+    FEMALE
+}
+
+@Serializable
+data class BirthDate(
+    val date: String = "",
+    val calendarType: CalendarType = CalendarType.SOLAR,
+)
+
+@Serializable
+enum class CalendarType {
+    @SerialName("SOLAR")
+    SOLAR,
+
+    @SerialName("LUNAR")
+    LUNAR,
+}
+
+@Serializable
+enum class MissionCategory {
+    @SerialName("TRANSPORTATION")
+    TRANSPORTATION,
+
+    @SerialName("FOOD")
+    FOOD,
+
+    @SerialName("DIGITAL")
+    DIGITAL,
+
+    @SerialName("SHOPPING")
+    SHOPPING,
+
+    @SerialName("TRAVEL")
+    TRAVEL,
+
+    @SerialName("SOCIAL")
+    SOCIAL,
+}
