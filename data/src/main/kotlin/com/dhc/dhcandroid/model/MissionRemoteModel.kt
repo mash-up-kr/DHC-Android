425c9dc5e76951426f087f453eeb90b18d438c7f
fbghgus123 <fbghgus123@naver.com>
2025-06-24 21:20:56 +0900

    [#208] Feature : MissionRemoteModel 생성

---
+++ data/src/main/kotlin/com/dhc/dhcandroid/model/MissionRemoteModel.kt
@@ -0,0 +1,26 @@
+package com.dhc.dhcandroid.model
+
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class MissionRemoteModel(
+    val missionId: String = "",
+    val category: MissionCategory = MissionCategory.TRANSPORTATION,
+    val difficulty: Int = 0,
+    val type: MissionType = MissionType.LONG_TERM,
+    val finished: Boolean = false,
+    val cost: String = "", // 이거는 왜 string 으로 내려올까?
+    val endDate: String = "",
+    val title: String = "",
+    val switchCount: Int = 0,
+)
+
+@Serializable
+enum class MissionType {
+    @SerialName("LONG_TERM")
+    LONG_TERM,
+
+    @SerialName("DAILY")
+    DAILY,
+}
