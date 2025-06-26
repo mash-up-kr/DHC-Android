a8d4fceafc50159d7e9a1c559926408dc79686fb
fbghgus123 <fbghgus123@naver.com>
2025-06-26 23:35:59 +0900

    [#227] Chore : DhcRemoteRepository 내용 추가

--- data/src/main/kotlin/com/dhc/dhcandroid/repository/DhcRemoteRepositoryImpl.kt
+++ data/src/main/kotlin/com/dhc/dhcandroid/repository/DhcRemoteRepositoryImpl.kt
@@ -1,10 +1,51 @@
 package com.dhc.dhcandroid.repository
 
+import com.dhc.common.DhcResult
 import com.dhc.dhcandroid.datasource.DhcRemoteDataSource
+import com.dhc.dhcandroid.model.AnalysisViewResponse
+import com.dhc.dhcandroid.model.EndTodayMissionRequest
+import com.dhc.dhcandroid.model.EndTodayMissionResponse
+import com.dhc.dhcandroid.model.HomeViewResponse
+import com.dhc.dhcandroid.model.LogoutResponse
+import com.dhc.dhcandroid.model.Mission
+import com.dhc.dhcandroid.model.MyPageResponse
+import com.dhc.dhcandroid.model.RegisterUserResponse
+import com.dhc.dhcandroid.model.ToggleMissionRequest
+import com.dhc.dhcandroid.model.UserProfile
+import com.dhc.dhcandroid.util.runDhcCatching
 import javax.inject.Inject
 
 class DhcRemoteRepositoryImpl @Inject constructor(
     private val dhcRemoteDataSource: DhcRemoteDataSource,
-): DhcRemoteRepository {
+) : DhcRemoteRepository {
+    override suspend fun registerUser(userProfile: UserProfile): DhcResult<RegisterUserResponse> =
+        runDhcCatching { dhcRemoteDataSource.registerUser(userProfile) }
 
+    override suspend fun changeMissionStatus(
+        userId: String,
+        missionId: String,
+        toggleMissionRequest: ToggleMissionRequest
+    ): DhcResult<Mission> =
+        runDhcCatching {
+            dhcRemoteDataSource.changeMissionStatus(
+                userId,
+                missionId,
+                toggleMissionRequest
+            )
+        }
+
+    override suspend fun requestFinishTodayMissions(endTodayMissionRequest: EndTodayMissionRequest): DhcResult<EndTodayMissionResponse> =
+        runDhcCatching { dhcRemoteDataSource.requestFinishTodayMissions(endTodayMissionRequest) }
+
+    override suspend fun requestLogOutUser(userId: String): DhcResult<LogoutResponse> =
+        runDhcCatching { dhcRemoteDataSource.requestLogOutUser(userId) }
+
+    override suspend fun getHomeView(userId: String): DhcResult<HomeViewResponse> =
+        runDhcCatching { dhcRemoteDataSource.getHomeView(userId) }
+
+    override suspend fun getMyPageView(userId: String): DhcResult<MyPageResponse> =
+        runDhcCatching { dhcRemoteDataSource.getMyPageView(userId) }
+
+    override suspend fun getAnalysisView(userId: String): DhcResult<AnalysisViewResponse> =
+        runDhcCatching { dhcRemoteDataSource.getAnalysisView(userId) }
 }
