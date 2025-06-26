32d0e3927f40cdbe268d9972cc08b18d88b23e4d
fbghgus123 <fbghgus123@naver.com>
2025-06-26 23:33:03 +0900

    [#227] Chore : DhcRemoteDataSource 내용 추가

--- data/src/main/kotlin/com/dhc/dhcandroid/datasource/DhcRemoteDataSourceImpl.kt
+++ data/src/main/kotlin/com/dhc/dhcandroid/datasource/DhcRemoteDataSourceImpl.kt
@@ -1,10 +1,52 @@
 package com.dhc.dhcandroid.datasource
 
+import com.dhc.dhcandroid.model.AnalysisViewResponse
+import com.dhc.dhcandroid.model.EndTodayMissionRequest
+import com.dhc.dhcandroid.model.EndTodayMissionResponse
+import com.dhc.dhcandroid.model.HomeViewResponse
+import com.dhc.dhcandroid.model.LogoutResponse
+import com.dhc.dhcandroid.model.Mission
+import com.dhc.dhcandroid.model.MyPageResponse
+import com.dhc.dhcandroid.model.RegisterUserResponse
+import com.dhc.dhcandroid.model.ToggleMissionRequest
+import com.dhc.dhcandroid.model.UserProfile
 import com.dhc.dhcandroid.service.DhcService
+import retrofit2.Response
 import javax.inject.Inject
 
 class DhcRemoteDataSourceImpl @Inject constructor(
     private val dhcService: DhcService,
 ): DhcRemoteDataSource {
+    override suspend fun registerUser(userProfile: UserProfile): Response<RegisterUserResponse> {
+        return dhcService.registerUser(userProfile)
+    }
+
+    override suspend fun changeMissionStatus(
+        userId: String,
+        missionId: String,
+        toggleMissionRequest: ToggleMissionRequest
+    ): Response<Mission> {
+        return dhcService.changeMissionStatus(userId, missionId, toggleMissionRequest)
+    }
+
+    override suspend fun requestFinishTodayMissions(endTodayMissionRequest: EndTodayMissionRequest): Response<EndTodayMissionResponse> {
+        return dhcService.requestFinishTodayMissions(endTodayMissionRequest)
+    }
+
+    override suspend fun requestLogOutUser(userId: String): Response<LogoutResponse> {
+        return dhcService.requestLogOutUser(userId)
+    }
+
+    override suspend fun getHomeView(userId: String): Response<HomeViewResponse> {
+        return dhcService.getHomeView(userId)
+    }
+
+    override suspend fun getMyPageView(userId: String): Response<MyPageResponse> {
+        return dhcService.getMyPageView(userId)
+    }
+
+    override suspend fun getAnalysisView(userId: String): Response<AnalysisViewResponse> {
+        return dhcService.getAnalysisView(userId)
+    }
 
 }
