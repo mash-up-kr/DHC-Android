078f571527fe2c370f5de95f5e727f4893e196c9
JeonK1 <kan5016@naver.com>
2025-06-28 00:51:57 +0900

    [#225] DhcFloatingButton 디자인 개편

--- core/designsystem/src/main/kotlin/com/dhc/designsystem/floatingButton/DhcFloatingButton.kt
+++ core/designsystem/src/main/kotlin/com/dhc/designsystem/floatingButton/DhcFloatingButton.kt
@@ -1,19 +1,24 @@
 package com.dhc.designsystem.floatingButton
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.tooling.preview.PreviewParameter
 import androidx.compose.ui.tooling.preview.PreviewParameterProvider
 import androidx.compose.ui.unit.dp
 import com.dhc.common.FullRoundedCornerShape
 import com.dhc.common.clickableIf
+import com.dhc.common.toPx
 import com.dhc.designsystem.AccentColor
 import com.dhc.designsystem.DhcTypoTokens
+import com.dhc.designsystem.GradientColor
 import com.dhc.designsystem.LocalDhcColors
 import com.dhc.designsystem.SurfaceColor
 
@@ -34,8 +39,24 @@ fun DhcFloatingButton(
                 elevation = 4.dp,
                 shape = FullRoundedCornerShape,
             )
+            .background(color = AccentColor.violet500)
             .background(
-                color = if (isEnabled) AccentColor.violet400 else SurfaceColor.neutral500,
+                brush = if (isEnabled) {
+                    GradientColor.buttonSurfaceGradient02(
+                        centerOffset = Offset(
+                            x = 72.dp.toPx(),
+                            y = 58.dp.toPx(),
+                        ),
+                        radius = 130f,
+                    )
+                } else {
+                    Brush.linearGradient(colors = listOf(SurfaceColor.neutral500))
+                },
+                shape = FullRoundedCornerShape,
+            )
+            .border(
+                width = 1.dp,
+                brush = GradientColor.buttonBorderGradient01,
                 shape = FullRoundedCornerShape,
             )
             .clickableIf(predicate = { isEnabled }) { onClick() }
