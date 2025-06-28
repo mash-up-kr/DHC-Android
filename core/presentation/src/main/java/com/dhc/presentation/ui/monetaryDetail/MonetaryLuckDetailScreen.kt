58dc76a60a9ecfcf207ef6723ea5178344e8929f
ddyeon <cab05102@naver.com>
2025-06-28 20:30:36 +0900

    [#215] Fix : 리뷰 반영

--- core/presentation/src/main/java/com/dhc/presentation/ui/monetaryDetail/MonetaryLuckDetailScreen.kt
+++ core/presentation/src/main/java/com/dhc/presentation/ui/monetaryDetail/MonetaryLuckDetailScreen.kt
@@ -16,12 +16,10 @@ import androidx.compose.foundation.verticalScroll
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import com.dhc.designsystem.AccentColor
 import com.dhc.designsystem.DhcTheme
 import com.dhc.designsystem.DhcTypoTokens
 import com.dhc.designsystem.GradientColor.buttonGradient
@@ -85,7 +83,7 @@ fun MonetaryLuckDetailScreen(
                         .fillMaxWidth()
                         .height(40.dp)
                         .background(brush = buttonGradient)
-                ) {}
+                )
                 Box(
                     modifier = Modifier
                         .fillMaxWidth()
