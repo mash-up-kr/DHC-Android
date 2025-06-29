17da11b227998ba34694501e954a02bdb1f3e9fe
JeonK1 <kan5016@naver.com>
2025-06-29 19:32:32 +0900

    [#243] DhcFortuneCard imageUrl, painterResource 모두 가능하도록 확장

--- core/designsystem/src/main/kotlin/com/dhc/designsystem/fortunecard/DhcFortuneCard.kt
+++ core/designsystem/src/main/kotlin/com/dhc/designsystem/fortunecard/DhcFortuneCard.kt
@@ -6,6 +6,7 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.BoxScope
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
@@ -17,12 +18,14 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.tooling.preview.PreviewParameter
 import androidx.compose.ui.tooling.preview.PreviewParameterProvider
 import androidx.compose.ui.unit.dp
+import coil3.compose.AsyncImage
 import com.dhc.designsystem.DhcTheme
 import com.dhc.designsystem.DhcTypoTokens
 import com.dhc.designsystem.GradientColor
@@ -33,8 +36,68 @@ import com.dhc.designsystem.SurfaceColor
 fun DhcFortuneCard(
     title: String,
     description: String,
+    imageUrl: String?,
+    modifier: Modifier = Modifier,
+) {
+    DhcFortuneCardInternal(
+        title = title,
+        description = description,
+        modifier = modifier.size(143.dp, 197.dp),
+        backgroundContent = {
+            if (imageUrl != null) {
+                AsyncImage(
+                    model = imageUrl,
+                    contentDescription = "운세카드 이미지",
+                    contentScale = ContentScale.Crop,
+                    modifier = Modifier.fillMaxSize(),
+                )
+            } else {
+                Spacer(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .background(SurfaceColor.neutral700),
+                )
+            }
+        },
+    )
+}
+
+@Composable
+fun DhcFortuneCard(
+    title: String,
+    description: String,
     modifier: Modifier = Modifier,
     @DrawableRes cardDrawableResId: Int? = null, // Todo : default value 빼줄 예정
+) {
+    DhcFortuneCardInternal(
+        title = title,
+        description = description,
+        modifier = modifier.size(143.dp, 197.dp),
+        backgroundContent = {
+            if (cardDrawableResId != null) {
+                Image(
+                    painter = painterResource(cardDrawableResId),
+                    contentDescription = "운세카드 이미지",
+                    contentScale = ContentScale.Crop,
+                    modifier = Modifier.fillMaxSize(),
+                )
+            } else {
+                Spacer(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .background(SurfaceColor.neutral700),
+                )
+            }
+        },
+    )
+}
+
+@Composable
+fun DhcFortuneCardInternal(
+    title: String,
+    description: String,
+    modifier: Modifier = Modifier,
+    backgroundContent: @Composable BoxScope.() -> Unit,
 ) {
     val colors = LocalDhcColors.current
     Box(
@@ -55,19 +118,7 @@ fun DhcFortuneCard(
                 shape = RoundedCornerShape(12.dp),
             ),
     ) {
-        if (cardDrawableResId != null) {
-            Image(
-                painter = painterResource(cardDrawableResId),
-                contentDescription = "운세카드 이미지",
-                modifier = Modifier.fillMaxSize(),
-            )
-        } else {
-            Spacer(
-                modifier = Modifier
-                    .fillMaxSize()
-                    .background(SurfaceColor.neutral700),
-            )
-        }
+        backgroundContent()
         Column(
             modifier = Modifier
                 .fillMaxSize()
