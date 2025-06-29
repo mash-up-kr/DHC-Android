50eb97701006a7dccbb13ec609bdca33f31bf273
JeonK1 <kan5016@naver.com>
2025-06-29 19:20:33 +0900

    [#243] 인트로에서 운세카드 뒷면카드에 이미지 설정

--- core/designsystem/src/main/kotlin/com/dhc/designsystem/fortunecard/DhcFortuneCard.kt
+++ core/designsystem/src/main/kotlin/com/dhc/designsystem/fortunecard/DhcFortuneCard.kt
@@ -1,56 +1,46 @@
 package com.dhc.designsystem.fortunecard
 
+import androidx.annotation.DrawableRes
 import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
 import androidx.compose.ui.draw.shadow
-import androidx.compose.ui.graphics.Brush
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.tooling.preview.PreviewParameter
 import androidx.compose.ui.tooling.preview.PreviewParameterProvider
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
-import com.dhc.common.FullRoundedCornerShape
-import com.dhc.designsystem.AccentColor
 import com.dhc.designsystem.DhcTheme
 import com.dhc.designsystem.DhcTypoTokens
-import com.dhc.designsystem.DhcTypoTokens.typoFontFamily
 import com.dhc.designsystem.GradientColor
 import com.dhc.designsystem.LocalDhcColors
 import com.dhc.designsystem.SurfaceColor
-import com.dhc.designsystem.colors
 
 @Composable
 fun DhcFortuneCard(
     title: String,
     description: String,
     modifier: Modifier = Modifier,
+    @DrawableRes cardDrawableResId: Int? = null, // Todo : default value 빼줄 예정
 ) {
     val colors = LocalDhcColors.current
-    Column(
+    Box(
         modifier = modifier
             .shadow(
                 elevation = 2.dp,
-                shape = RoundedCornerShape(12.dp),
-            )
-            .background(
-                color = SurfaceColor.neutral700,
                 shape = RoundedCornerShape(12.dp),
             )
             .border(
@@ -63,22 +53,40 @@ fun DhcFortuneCard(
             .shadow(
                 elevation = 2.dp,
                 shape = RoundedCornerShape(12.dp),
-            )
-            .padding(vertical = 20.dp, horizontal = 24.dp),
-        horizontalAlignment = Alignment.CenterHorizontally,
+            ),
     ) {
-        Text(
-            text = title,
-            style = DhcTypoTokens.TitleH8.copy(brush = GradientColor.textGradient02),
-            textAlign = TextAlign.Center,
-        )
-        Text(
-            modifier = Modifier.padding(bottom = 8.dp),
-            text = description,
-            style = DhcTypoTokens.TitleH6,
-            textAlign = TextAlign.Center,
-            color = colors.text.textBodyPrimary,
-        )
+        if (cardDrawableResId != null) {
+            Image(
+                painter = painterResource(cardDrawableResId),
+                contentDescription = "운세카드 이미지",
+                modifier = Modifier.fillMaxSize(),
+            )
+        } else {
+            Spacer(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .background(SurfaceColor.neutral700),
+            )
+        }
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(vertical = 20.dp, horizontal = 24.dp),
+            horizontalAlignment = Alignment.CenterHorizontally,
+        ) {
+            Text(
+                text = title,
+                style = DhcTypoTokens.TitleH8.copy(brush = GradientColor.textGradient02),
+                textAlign = TextAlign.Center,
+            )
+            Text(
+                modifier = Modifier.padding(bottom = 8.dp),
+                text = description,
+                style = DhcTypoTokens.TitleH6,
+                textAlign = TextAlign.Center,
+                color = colors.text.textBodyPrimary,
+            )
+        }
     }
 }
 
