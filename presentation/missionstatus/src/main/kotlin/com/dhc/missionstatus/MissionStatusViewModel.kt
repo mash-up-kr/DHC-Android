3523196cfb9a23c5459b8bd5e322a73ef437d602
fbghgus123 <fbghgus123@naver.com>
2025-06-29 16:17:56 +0900

    [#250] Feature : 캘린더 넘기면 데이터 불러오기 구현

--- presentation/missionstatus/src/main/kotlin/com/dhc/missionstatus/MissionStatusViewModel.kt
+++ presentation/missionstatus/src/main/kotlin/com/dhc/missionstatus/MissionStatusViewModel.kt
@@ -1,7 +1,10 @@
 package com.dhc.missionstatus
 
 import androidx.lifecycle.viewModelScope
+import com.dhc.common.getSuccessOrNull
 import com.dhc.common.onSuccess
+import com.dhc.designsystem.calendar.model.DhcCalendarDayData
+import com.dhc.designsystem.calendar.model.DhcCalendarMonthData
 import com.dhc.dhcandroid.repository.AuthDataStoreRepository
 import com.dhc.dhcandroid.repository.DhcRepository
 import com.dhc.missionstatus.MissionStatusContract.Event
@@ -12,6 +15,7 @@ import com.dhc.presentation.mvi.BaseViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.firstOrNull
 import kotlinx.coroutines.launch
+import java.time.LocalDate
 import javax.inject.Inject
 
 @HiltViewModel
@@ -34,5 +38,26 @@ class MissionStatusViewModel @Inject constructor(
                 data ?: return@onSuccess // Todo :: null 응답 관련 처리 필요
                 reduce { copy(consumptionAnalysisUiModel = ConsumptionAnalysisUiModel.from(data)) }
             }
+    }
+
+    suspend fun getCalendarData(yearMonth: LocalDate): Map<LocalDate, DhcCalendarMonthData> {
+        val userId = authRepository.getUUID().firstOrNull().orEmpty()
+        return dhcRepository.getCalendarView(userId, yearMonth)
+            .getSuccessOrNull()
+            ?.threeMonthViewResponse
+            ?.mapIndexed { index, data ->
+                val month = yearMonth.plusMonths(index - 1L)
+                month to DhcCalendarMonthData(
+                    yearMonth = month,
+                    data = data.calendarDayMissionViews.associate { dayMissionView ->
+                        dayMissionView.day to DhcCalendarDayData(
+                            finishedMissionCount = dayMissionView.finishedMissionCount,
+                        )
+                    }
+                )
+            }
+            ?.toMap()
+            .orEmpty()
+
     }
 }
