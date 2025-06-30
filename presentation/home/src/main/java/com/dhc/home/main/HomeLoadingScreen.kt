77e1cb33c459d77370ddb3b163246c0ebb178650
JeonK1 <kan5016@naver.com>
2025-07-01 02:02:08 +0900

    [#274] Feature : LoadingScreen 추가 개발

--- presentation/home/src/main/java/com/dhc/home/main/HomeLoadingScreen.kt
+++ presentation/home/src/main/java/com/dhc/home/main/HomeLoadingScreen.kt
@@ -1,20 +1,28 @@
 package com.dhc.home.main
 
+import androidx.compose.foundation.Canvas
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.statusBars
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.dhc.designsystem.DhcTheme
 import com.dhc.designsystem.DhcTypoTokens
+import com.dhc.designsystem.GradientColor
 import com.dhc.designsystem.LocalDhcColors
 import com.dhc.designsystem.SurfaceColor
 import com.dhc.designsystem.fortunecard.DhcFortuneCard
@@ -24,6 +32,15 @@ import com.dhc.home.R
 @Composable
 fun HomeLoadingScreen(modifier: Modifier = Modifier) {
     val colors = LocalDhcColors.current
+    val density = LocalDensity.current
+    val topBarSize = WindowInsets.statusBars.getTop(density)
+    Box(
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(466.dp)
+            .offset(y = -(topBarSize.div(density.density).dp))
+            .background(brush = GradientColor.backgroundGradient02Alpha(0.6f))
+    )
     Column(
         modifier = modifier.padding(horizontal = 20.dp),
         horizontalAlignment = Alignment.CenterHorizontally,
@@ -46,29 +63,40 @@ fun HomeLoadingScreen(modifier: Modifier = Modifier) {
         )
         Spacer(modifier = Modifier.height(68.dp))
 
-        // Todo : 로띠 애니메이션이 들어갈 예정
-        FlippableBox(
-            isFlipped = false,
-            onFlipEnd = {},
-            frontScreen = {
-                DhcFortuneCard(
-                    title = "최고의 날",
-                    description = "네잎클로버",
-                    cardDrawableResId = R.drawable.fortune_card_sample,
-                    modifier = Modifier.size(width = 143.dp, height = 197.dp),
+        Box(modifier = Modifier.fillMaxWidth()) {
+            // Todo : 로띠 애니메이션이 들어갈 예정
+            val canvasBackgroundBrush = GradientColor.backgroundGradient01(radius = 350f)
+            Canvas(modifier = Modifier.matchParentSize()) {
+                drawOval(
+                    brush = canvasBackgroundBrush,
+                    size = size,
+                    alpha = 0.6f,
                 )
-            },
-            backScreen = {
-                DhcFortuneCard(
-                    title = "최고의 날",
-                    description = "네잎클로버",
-                    cardDrawableResId = R.drawable.fortune_card_sample,
-                    modifier = Modifier.size(width = 143.dp, height = 197.dp),
-                )
-            },
-            initialRotationZ = -4f,
-            isEnabled = false,
-        )
+            }
+            FlippableBox(
+                isFlipped = false,
+                onFlipEnd = {},
+                frontScreen = {
+                    DhcFortuneCard(
+                        title = "최고의 날",
+                        description = "네잎클로버",
+                        cardDrawableResId = R.drawable.fortune_card_sample,
+                        modifier = Modifier.size(width = 143.dp, height = 197.dp),
+                    )
+                },
+                backScreen = {
+                    DhcFortuneCard(
+                        title = "최고의 날",
+                        description = "네잎클로버",
+                        cardDrawableResId = R.drawable.fortune_card_sample,
+                        modifier = Modifier.size(width = 143.dp, height = 197.dp),
+                    )
+                },
+                modifier = Modifier.align(Alignment.Center),
+                initialRotationZ = -4f,
+                isEnabled = false,
+            )
+        }
     }
 }
 
