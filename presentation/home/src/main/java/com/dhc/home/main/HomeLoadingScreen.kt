c7914ae66e312dfa528ae299ac1a50f0e358f50e
JeonK1 <kan5016@naver.com>
2025-07-02 01:57:39 +0900

    [#306] Feature : HomeLoadingScreen 에 동영상 적용하기

--- presentation/home/src/main/java/com/dhc/home/main/HomeLoadingScreen.kt
+++ presentation/home/src/main/java/com/dhc/home/main/HomeLoadingScreen.kt
@@ -1,33 +1,33 @@
 package com.dhc.home.main
 
-import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.statusBars
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalDensity
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.dhc.common.CalendarUtil
 import com.dhc.designsystem.DhcTheme
 import com.dhc.designsystem.DhcTypoTokens
 import com.dhc.designsystem.GradientColor
 import com.dhc.designsystem.LocalDhcColors
 import com.dhc.designsystem.SurfaceColor
-import com.dhc.designsystem.fortunecard.DhcFortuneCard
-import com.dhc.designsystem.fortunecard.FlippableBox
 import com.dhc.home.R
+import com.dhc.presentation.component.VideoView
 
 @Composable
 fun HomeLoadingScreen(modifier: Modifier = Modifier) {
@@ -41,60 +41,34 @@ fun HomeLoadingScreen(modifier: Modifier = Modifier) {
             .offset(y = -(topBarSize.div(density.density).dp))
             .background(brush = GradientColor.backgroundGradient02Alpha(0.6f))
     )
-    Column(
-        modifier = modifier.padding(horizontal = 20.dp),
-        horizontalAlignment = Alignment.CenterHorizontally,
-    ) {
-        Spacer(modifier = Modifier.height(84.dp))
-        Text(
-            text = "오늘의 운세를 카드에 담고 있어요..",
-            color = colors.text.textBodyPrimary,
-            style = DhcTypoTokens.TitleH4,
-            textAlign = TextAlign.Center,
-            modifier = Modifier.fillMaxWidth(),
+    Box(modifier = modifier.fillMaxSize()) {
+        VideoView(
+            videoResId = R.raw.loading_video,
+            thumbnailResId = null, // Todo : 추후 썸네일 반영 예정
+            modifier = Modifier
+                .fillMaxSize()
+                .offset(y = -(topBarSize.div(density.density).dp)),
         )
-        Spacer(modifier = Modifier.height(8.dp))
-        Text(
-            text = "5월 21일",
-            color = SurfaceColor.neutral300,
-            style = DhcTypoTokens.Body3,
-            textAlign = TextAlign.Center,
-            modifier = Modifier.fillMaxWidth(),
-        )
-        Spacer(modifier = Modifier.height(68.dp))
 
-        Box(modifier = Modifier.fillMaxWidth()) {
-            // Todo : 로띠 애니메이션이 들어갈 예정
-            val canvasBackgroundBrush = GradientColor.backgroundGradient01(radius = 350f)
-            Canvas(modifier = Modifier.matchParentSize()) {
-                drawOval(
-                    brush = canvasBackgroundBrush,
-                    size = size,
-                    alpha = 0.6f,
-                )
-            }
-            FlippableBox(
-                isFlipped = false,
-                onFlipEnd = {},
-                frontScreen = {
-                    DhcFortuneCard(
-                        title = "최고의 날",
-                        description = "네잎클로버",
-                        cardDrawableResId = R.drawable.fortune_card_sample,
-                        modifier = Modifier.size(width = 143.dp, height = 197.dp),
-                    )
-                },
-                backScreen = {
-                    DhcFortuneCard(
-                        title = "최고의 날",
-                        description = "네잎클로버",
-                        cardDrawableResId = R.drawable.fortune_card_sample,
-                        modifier = Modifier.size(width = 143.dp, height = 197.dp),
-                    )
-                },
-                modifier = Modifier.align(Alignment.Center),
-                initialRotationZ = -4f,
-                isEnabled = false,
+        Column(
+            modifier = Modifier.padding(horizontal = 20.dp),
+            horizontalAlignment = Alignment.CenterHorizontally,
+        ) {
+            Spacer(modifier = Modifier.height(84.dp))
+            Text(
+                text = CalendarUtil.getCurrentDate().let { stringResource(R.string.m_month_d_day, it.month.value, it.dayOfMonth)},
+                color = SurfaceColor.neutral300,
+                style = DhcTypoTokens.Body3,
+                textAlign = TextAlign.Center,
+                modifier = Modifier.fillMaxWidth(),
+            )
+            Spacer(modifier = Modifier.height(8.dp))
+            Text(
+                text = stringResource(R.string.loading_today_fortune_card),
+                color = colors.text.textBodyPrimary,
+                style = DhcTypoTokens.TitleH4,
+                textAlign = TextAlign.Center,
+                modifier = Modifier.fillMaxWidth(),
             )
         }
     }
