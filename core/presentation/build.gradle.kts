f0839e39867dc51f384b6f9050cf26cedc4abba2
JeonK1 <kan5016@naver.com>
2025-07-02 02:27:40 +0900

    [#284] Lottie 추가

--- core/presentation/build.gradle.kts
+++ core/presentation/build.gradle.kts
@@ -13,5 +13,6 @@ dependencies {
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.viewmodel.android)
     implementation(libs.firebase.messaging.ktx)
+    implementation(libs.lottie.compose)
     testImplementation(libs.junit)
 }
