5ce06e9195f9a9c97f7b6917ba8a034ed56c977a
fbghgus123 <fbghgus123@naver.com>
2025-07-02 21:45:53 +0900

    [#270] Feature : 금전운 상세 서버 데이터 연동

---
+++ presentation/home/src/main/java/com/dhc/home/detail/utils/Extensions.kt
@@ -0,0 +1,48 @@
+package com.dhc.home.detail.utils
+
+import com.dhc.common.FormatterUtil
+import com.dhc.designsystem.tipcard.TipCardModel
+import com.dhc.dhcandroid.model.FortuneResponse
+import com.dhc.presentation.ui.monetaryDetail.FortuneCard
+import com.dhc.presentation.ui.monetaryDetail.MonetaryLuckInfo
+import com.dhc.presentation.ui.monetaryDetail.ScoreInfo
+import java.time.LocalDate
+
+internal fun FortuneResponse.toMonetaryLuckInfo() = MonetaryLuckInfo(
+    scoreInfo = ScoreInfo(
+        date = LocalDate.parse(this.date, FormatterUtil.dhcYearMonthDayFormat)
+            .format(FormatterUtil.dhcDateFormat),
+        score = this.totalScore,
+        description = this.fortuneTitle,
+    ),
+    fortuneCard = FortuneCard(
+        message = "" // Todo :: 카드 메세지 필요
+    ),
+    monetaryDetail = this.fortuneDetail,
+    todayTips = listOf(
+        TipCardModel(
+            title = "오늘의 추천 메뉴",
+            icon = todayMenuImageUrl,
+            cont = todayMenu,
+            color = null,
+        ),
+        TipCardModel(
+            title = "행운의 색상",
+            icon = luckyColorImageUrl,
+            cont = luckyColor,
+            color = luckyColorHex,
+        ),
+        TipCardModel(
+            title = "피해야 할 음식",
+            icon = jinxedMenuImageUrl,
+            cont = jinxedMenu,
+            color = null,
+        ),
+        TipCardModel(
+            title = "피해야 할 색상",
+            icon = jinxedColorImageUrl,
+            cont = jinxedColor,
+            color = jinxedColorHex,
+        ),
+    )
+)
