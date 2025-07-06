4c4eae0308b300b0b1645ee5599aeed5bbe8eb6f
JeonK1 <kan5016@naver.com>
2025-07-06 16:30:53 +0900

    [#294] TopBar 랑 BottomBar 에 대해서 생겨나고 사라질 때 애니메이션 적용하기

--- core/designsystem/src/main/kotlin/com/dhc/designsystem/gnb/DhcBottomBar.kt
+++ core/designsystem/src/main/kotlin/com/dhc/designsystem/gnb/DhcBottomBar.kt
@@ -1,5 +1,9 @@
 package com.dhc.designsystem.gnb
 
+import androidx.compose.animation.AnimatedContent
+import androidx.compose.animation.slideInVertically
+import androidx.compose.animation.slideOutVertically
+import androidx.compose.animation.togetherWith
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import com.dhc.designsystem.gnb.model.DhcBottomBarState
@@ -11,24 +15,35 @@ fun DhcBottomBar(
     navigateToRoute: (String) -> Unit,
     modifier: Modifier = Modifier,
 ) {
-
     val items = DhcBottomBarState.BottomNavigation.items
     val selectedIndex = items
         .indexOfFirst { it.routeName == currentRoute }
         .coerceAtLeast(0)
 
-    when (state) {
-        is DhcBottomBarState.BottomNavigation -> {
-            DhcGnb(
-                gnbItemList = items,
-                selectedIndex = selectedIndex,
-                onClickItem = { gnbItem, index ->
-                    navigateToRoute(gnbItem.routeName)
-                },
-                modifier = modifier,
+    AnimatedContent(
+        targetState = state,
+        transitionSpec = {
+            slideInVertically(
+                initialOffsetY = { it }
+            ) togetherWith slideOutVertically(
+                targetOffsetY = { it }
             )
-        }
+        },
+        contentKey = { currentState -> currentState::class.java.simpleName },
+    ) { currentState ->
+        when (currentState) {
+            is DhcBottomBarState.BottomNavigation -> {
+                DhcGnb(
+                    gnbItemList = items,
+                    selectedIndex = selectedIndex,
+                    onClickItem = { gnbItem, _ ->
+                        navigateToRoute(gnbItem.routeName)
+                    },
+                    modifier = modifier,
+                )
+            }
 
-        is DhcBottomBarState.None -> Unit
+            is DhcBottomBarState.None -> Unit
+        }
     }
 }
