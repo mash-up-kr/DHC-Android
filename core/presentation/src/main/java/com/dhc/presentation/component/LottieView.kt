bb53f94ff6c2482a2d6ebf0c59d74b79c6919496
JeonK1 <kan5016@naver.com>
2025-07-07 23:39:43 +0900

    [#355] Feature : LottieView Crop 적용

--- core/presentation/src/main/java/com/dhc/presentation/component/LottieView.kt
+++ core/presentation/src/main/java/com/dhc/presentation/component/LottieView.kt
@@ -4,6 +4,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
 import com.airbnb.lottie.compose.LottieAnimation
 import com.airbnb.lottie.compose.LottieCompositionSpec
 import com.airbnb.lottie.compose.animateLottieCompositionAsState
@@ -14,6 +15,7 @@ fun LottieView(
     assetName: String,
     modifier: Modifier = Modifier,
     iterations: Int = Int.MAX_VALUE,
+    contentScale: ContentScale = ContentScale.Crop,
     lottieFinished: () -> Unit = {},
 ) {
     val composition by rememberLottieComposition(LottieCompositionSpec.Asset(assetName))
@@ -27,6 +29,7 @@ fun LottieView(
     LottieAnimation(
         composition = composition,
         progress = { progress },
+        contentScale = contentScale,
         modifier = modifier,
     )
 }
