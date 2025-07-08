6dd8616d97f1750519af70d7255d881417787939
JeonK1 <kan5016@naver.com>
2025-07-09 00:09:49 +0900

    [#355] Basic -> None 으로 변경될 때 scaleOut 으로 사라지는 이슈 수정

--- core/designsystem/src/main/kotlin/com/dhc/designsystem/topbar/DhcTopBar.kt
+++ core/designsystem/src/main/kotlin/com/dhc/designsystem/topbar/DhcTopBar.kt
@@ -4,6 +4,8 @@ import androidx.compose.animation.AnimatedContent
 import androidx.compose.animation.slideInVertically
 import androidx.compose.animation.slideOutVertically
 import androidx.compose.animation.togetherWith
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import com.dhc.designsystem.topbar.model.DhcTopBarState
@@ -37,7 +39,10 @@ fun DhcTopBar(
                 )
             }
 
-            is DhcTopBarState.None -> Unit
+            is DhcTopBarState.None -> {
+                // Unit 으로 설정하면 Basic -> None 으로 갈 때 scaleOut 으로 사라지는 이슈가 있다.
+                Spacer(modifier = Modifier.fillMaxWidth())
+            }
         }
     }
 }
