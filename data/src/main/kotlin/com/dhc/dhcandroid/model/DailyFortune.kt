60d13b82e9aa3b9ed4fa52ade55fe401781d387f
ddyeon <cab05102@naver.com>
2025-07-08 13:33:44 +0900

    [#364] Fix : 금전운 확인 파라미터 수정

--- data/src/main/kotlin/com/dhc/dhcandroid/model/DailyFortune.kt
+++ data/src/main/kotlin/com/dhc/dhcandroid/model/DailyFortune.kt
@@ -9,11 +9,14 @@ data class DailyFortune(
     val fortuneDetail: String = "",
     val jinxedColor: String = "",
     val jinxedColorHex: String = "",
+    val jinxedColorImage: String = "",
     val jinxedMenu: String = "",
     val jinxedNumber: Int = 0,
     val luckyColor: String = "",
     val luckyColorHex: String = "",
+    val luckyColorImage: String = "",
+    val todayMenuImage: String = "",
     val luckyNumber: Int = 0,
-    val score: Int = 0,
+    val totalScore: Int = 0,
     val todayMenu: String = ""
 )
