396b645639153ec168629bddf1e770063f21cbc0
fbghgus123 <fbghgus123@naver.com>
2025-07-14 19:42:17 +0900

    [#429] Fix : 홈화면 - 카드 텍스트, 이미지 겹치는 현상 수정

--- presentation/home/src/main/java/com/dhc/home/main/HomeScreen.kt
+++ presentation/home/src/main/java/com/dhc/home/main/HomeScreen.kt
@@ -2,19 +2,14 @@ package com.dhc.home.main
 
 import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.ScrollState
-import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.statusBars
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Text
@@ -22,7 +17,6 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.graphicsLayer
-import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -33,7 +27,6 @@ import com.dhc.designsystem.SurfaceColor
 import com.dhc.designsystem.floatingButton.DhcFloatingButton
 import com.dhc.designsystem.fortunecard.DhcFortuneCard
 import com.dhc.home.R
-import com.dhc.designsystem.R as DR
 import com.dhc.home.model.SelectChangeMission
 import com.dhc.presentation.mvi.EventHandler
 
@@ -74,7 +67,7 @@ fun HomeScreen(
                         .align(Alignment.CenterHorizontally)
                         .graphicsLayer(rotationZ = 4f)
                         .clickable { eventHandler(HomeContract.Event.ClickFortuneCard) },
-                    title = state.homeInfo.todayDailyFortune.fortuneTitle,
+                    title = state.homeInfo.todayDailyFortune.fortuneCardTitle,
                     description = state.homeInfo.todayDailyFortune.fortuneCardSubTitle,
                     imageUrl = state.homeInfo.todayDailyFortune.fortuneCardImage,
                 )
