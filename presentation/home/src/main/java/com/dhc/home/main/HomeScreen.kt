232cc57aea70e707499e8b235eea03b4e43a06f4
JeonK1 <kan5016@naver.com>
2026-02-03 22:26:48 +0900

    [#489] Delete : unused import

--- presentation/home/src/main/java/com/dhc/home/main/HomeScreen.kt
+++ presentation/home/src/main/java/com/dhc/home/main/HomeScreen.kt
@@ -24,7 +24,6 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.dhc.common.ImageResource
@@ -32,7 +31,6 @@ import com.dhc.designsystem.DhcTheme
 import com.dhc.designsystem.DhcTypoTokens
 import com.dhc.designsystem.GradientColor
 import com.dhc.designsystem.SurfaceColor
-import com.dhc.designsystem.floatingButton.DhcFloatingButton
 import com.dhc.designsystem.fortunecard.DhcFortuneCard
 import com.dhc.designsystem.modal.DhcModal
 import com.dhc.home.R
